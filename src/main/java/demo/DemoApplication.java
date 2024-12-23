e3056fff21b93feb03b3e530a20de0b56173070c
Jannik <scholz.carlsfeld@gmail.com>
2024-12-23 21:40:18 +0100

    html richtig platzieren/fixen

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,9 +2,9 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +12,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String home() {
+		return "index"; // Verweist auf templates/index.html  (Controller verwenden)
+	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
