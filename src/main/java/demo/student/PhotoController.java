b056643081fe06adda432531090d909fbfd4c248
Jannik <scholz.carlsfeld@gmail.com>
2025-01-14 20:24:27 +0100

    updated Controller

--- src/main/java/demo/student/PhotoController.java
+++ src/main/java/demo/student/PhotoController.java
@@ -1,25 +1,35 @@
 package demo.student;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 @RestController
 public class PhotoController {
 
-    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+    private Map<String, Photo> db = new HashMap<>(){
+        {put("1", new Photo("1", "hello.jpg"));}
+    };
 
     @GetMapping("/")
    public String helloWorld() {
        return "Hello World";
    }
 
-   @GetMapping("/photos")
-    public List<Photo> getPhotos() {
-        return db;
+   @GetMapping("/photos/{id}")
+   //konkretes Photo aus Liste
+   public Photo getPhotos(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return photo;
    }
-
-
 }
